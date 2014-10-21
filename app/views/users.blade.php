6b71d05c702fb3792d76efefcae877832a3d90ad
Kate deBethune <kdebethune@gmail.com>
2014-10-21 15:40:32 -0400

    fixed presentation logic in users view

--- app/views/users.blade.php
+++ app/views/users.blade.php
@@ -8,8 +8,20 @@
 	{{ "<h2>You requested ".$num_requested." fake users:</h2>" }}
 	
 	@foreach($fake_users as $fake_user)
-		{{ $fake_user['name']."<br>".$fake_user['birthday']."<br>".$fake_user['profile']."<br><br>" }}
+		{{ $fake_user['name']."<br>" }}
+		
+		@if ( $fake_user['birthday'] != NULL )
+			{{ $fake_user['birthday']."<br>" }}
+		@endif
+		
+		@if ( $fake_user['profile'] != NULL )
+			{{ $fake_user['profile']."<br>" }}
+		@endif
+		
+		{{ "<br>" }}
+		
 	@endforeach
+	
 	
 	
 @stop
